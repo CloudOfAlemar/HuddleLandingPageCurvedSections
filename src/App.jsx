ce201b72b89bc7713a54ddce3daa5deb8f988400
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-11-13 08:36:51 -0800

    Created the Header, Main and Footer Components to separate code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
 
   return (
     <>
-     Hello World!
+     <Header />
+     <Main />
+     <Footer />
     </>
   )
 }
